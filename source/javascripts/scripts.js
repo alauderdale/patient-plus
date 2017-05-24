ea7efb402bfcf8dc92cd203f54b634885865fd46
Alex Lauderdale <alauderdale@Alexs-MacBook-Pro.local>
2017-05-24 01:24:55 -0700

    slider

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -45,28 +45,22 @@ $(document).ready(function(){
                   }
             }
         ]
-
-
     });  
 
 
     $('#team-accordion').on('show.bs.collapse', function () {
-
+        //fires when accordian is clicked
         // $('.nurse-slider').lightSlider().refresh;
-        $(window).trigger('resize');
+        // $(window).trigger('resize');
         // $('.nurse-slider-wrapper').addClass('out');
-
         // setTimeout(function(){
         //  $('.nurse-slider-wrapper').removeClass('out');
         // }, 200);
-
-
     })
 
     //main nav
     $(window).scroll(function() {    
         var scroll = $(window).scrollTop();
-
         // if (scroll >= 200) {
         //     $(".global-nav").addClass("dark-header");
         // } else {
