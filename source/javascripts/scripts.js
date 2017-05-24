92df260b85915c2b94db8efbb1206fb505da268c
Alex Lauderdale <alauderdale@Alexs-MacBook-Pro.local>
2017-05-24 00:54:05 -0700

    slider

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -14,9 +14,49 @@ $(document).ready(function(){
  //        }
  //   });
 
+
+
+    //tooltips
+
 	$('.tooltip-up').tooltip({html:true});
 
+    $('[data-toggle="tooltip"]').tooltip();
 
+
+    //lightslider
+
+    $('.nurse-slider').lightSlider({
+        gallery:true,
+        mode: 'fade',
+        thumbItem:16,
+        slideMargin:0,
+        addClass: 'nurse-slider-wrapper', 
+
+        responsive : [
+            {
+                breakpoint:800,
+                settings: {
+                    thumbItem:6,
+                  }
+            }
+        ]
+
+
+    });  
+
+
+    $('#team-accordion').on('show.bs.collapse', function () {
+
+        // $('.nurse-slider').lightSlider().refresh;
+        $(window).trigger('resize');
+        // $('.nurse-slider-wrapper').addClass('out');
+
+        // setTimeout(function(){
+        //  $('.nurse-slider-wrapper').removeClass('out');
+        // }, 200);
+
+
+    })
 
     //main nav
     $(window).scroll(function() {    
