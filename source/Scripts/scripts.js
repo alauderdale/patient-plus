063b6960a7ef68b4d1ebd9ef410c30ddc1daf288
Alex Lauderdale <alauderdale@Alexs-MacBook-Pro.local>
2017-06-15 23:23:18 -0600

    hovercard

--- source/Scripts/scripts.js
+++ source/Scripts/scripts.js
@@ -56,6 +56,38 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 
 
+    //popovers
+
+    $(".dr-hovercard-trigger").popover({
+        trigger: 'manual',
+        container: $(this).attr('id'),
+        placement: 'top',
+        html : true,
+        content: function() {
+          var content = $(this).attr("data-popover-content");
+          return $(content).children(".popover-body").html();
+        },
+        title: function() {
+          var title = $(this).attr("data-popover-content");
+          return $(title).children(".popover-heading").html();
+        }
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
+
+
+
     //lightgallery
 
 
